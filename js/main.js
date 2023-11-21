e6a209bcd093fccb4554fcfb43a538183070dd4e
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-11-21 07:54:06 -0500

    fixed errors
    debugged errors

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
           <stop offset="1" stop-color="#09002a"/>
         </linearGradient>
       </defs>
-      <path d="m.7,85.91c24.68,40.13,68.96,33.63,88.06,13.09,21.08-22.66,19.17-58.11-4.66-78.31C60.57.76,20.35,3.88.71,37.55c-.38-5.14-1.77-10.21,2.29-15.02C20.19,2.15,47.12-5.27,71.63,3.85c24.63,9.17,40.79,32.79,40.48,59.18-.31,25.93-17.51,49.19-42.61,57.61-24.21,8.12-51.59-.26-67.3-20.37-3.37-4.32-1.97-8.67-1.49-14.36Z" fill="url(#linear-gradient)" stroke-width="0"/>
+      <path d="m.7,85.91c24.68,40.13,68.96,33.63,88.06,13.09,21.08-22.66,19.17-58.11-4.66-78.31C60.57.76,20.35,3.88.71,37.55c-.38-5.14-1.77-10.21,2.29-15.02C20.19,2.15,47.12-5.27,71.63,3.85c24.63,9.17,40.79,32.79,40.48,59.18-.31,25.93-17.51,49.19-42.61,57.61-24.21,8.12-51.59-.26-67.3-20.37-3.37-4.32-1.97-8.67-1.49-14.36Z" fill="url(#linear-gradient)" stroke-width="0">
             <animateTransform 
                 attributeName="transform" 
                 attributeType="XML" 
@@ -31,6 +31,7 @@
                 from="0 0 90"
                 to="360 0 90" 
                 repeatCount="indefinite" />
+                </path>
               
         </svg>`;
 
@@ -56,7 +57,7 @@
     fetch(infoBox)
       .then(response => response.json())
       .then(data => {
-        removeSpinner(); // Remove the spinner
+        removeSpinner(); // Remove the spinner on call after load
         data.forEach((infoBox, index) => {
           let selected = document.querySelector(`#hotspot-${index + 1}`);
 
@@ -66,15 +67,25 @@
           const textElement = document.createElement('p');
           textElement.textContent = infoBox.description;
 
-          // const imageElement = document.createElement('img');
-          // imageElement.src = infoBox.thumbnail; // could not edit the image in api
+          const imageElement = document.createElement('img');
+          imageElement.src = '../images/earbud.jpg'; // could not edit api so created an element
 
           selected.appendChild(titleElement);
           selected.appendChild(textElement);
-          // selected.appendChild(imageElement);
+          selected.appendChild(imageElement);
         });
       })
-      .catch(error => console.error('Error loading infoBoxes:', error));
+      .catch(error => {
+
+        const errorMessageElement = document.createElement('h2');
+        errorMessageElement.textContent = '404, 404, 404. Please try again later.';
+
+        // Append the error message 
+        peopleCon.innerHTML = '';
+        peopleCon.appendChild(errorMessageElement);
+
+        console.error('Error:', error);
+      });
   }
 
   function loadMaterialInfo() {
@@ -97,7 +108,7 @@
           materialList.appendChild(clone);
         });
       })
-      .catch(error => console.error('Error loading materialListData:', error)); //effective in console, getting it on front 
+      .catch(error => console.error('Error loading materialListData:', error));
   }
 
   function showInfo() {
